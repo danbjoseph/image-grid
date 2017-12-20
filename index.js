8d3e8e2154cab093111714ee103973aa9055ff85
Dan Joseph <danbjoseph@gmail.com>
2017-12-20 11:50:13 -0500

    adding exif tag checks

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var fs = require('fs');
 var path = require('path');
 
 var geo = JSON.parse(fs.readFileSync("aois.geojson"));
-// TODO: filter out non jpg files, system files
 var aois = geo.features;
 var imgDir = "./images";
 var images = fs.readdirSync(imgDir);
+var outputFolder = "output"; // must exist
+var outputIdentifier = "output"; // prepended to each sub output folder
 
 var deleteOriginals = false;
 
 function placeImage(image, cb) {
+    // TODO: filter out non jpg files, system files???
     var readStream = fs.createReadStream(path.join(imgDir, image), { start: 0, end: 65635 });
     // The exif section of a jpeg file has a maximum size of 65535 
     // bytes and the section seems to always occur within the first 
@@ -25,28 +27,69 @@ function placeImage(image, cb) {
           var buf = Buffer.concat(bufs)
           var parser = require('exif-parser').create(buf);
           var tags = parser.parse().tags;
-          // TODO: run a check to make sure image has valid GPS?
-          var pt = turf.point([tags.GPSLongitude,tags.GPSLatitude]);
-          var count = 0;
-          for (var i = 0; i < aois.length; i++) {
-            var inside = turf.booleanPointInPolygon(pt, aois[i]);
-            if (inside) {
-              if (!aois[i].properties.images) {
-                // need to create the array to hold the image names if it doesn't exist yet
-                aois[i].properties.images = [];
+          
+          var pass = true;
+          // http://www.exiv2.org/tags.html
+          var latitudeRefs = ['N', 'S']; // Indicates whether the latitude is north or south latitude. The ASCII value 'N' indicates north latitude, and 'S' is south latitude.
+          if (latitudeRefs.indexOf(tags.GPSLatitudeRef) === -1) { 
+            console.log("ERROR: " + image);
+            console.log("GPSLatitudeRef: " + tags.GPSLatitudeRef);
+            pass = false; 
+          }
+          var longitudeRefs = ['E', 'W']; // Indicates whether the longitude is east or west longitude. ASCII 'E' indicates east longitude, and 'W' is west longitude.
+          if (longitudeRefs.indexOf(tags.GPSLongitudeRef) === -1) { 
+            console.log("ERROR: " + image);
+            console.log("GPSLongitudeRef: " + tags.GPSLongitudeRef);
+            pass = false;
+          }
+          if (tags.GPSAltitudeRef === undefined) { 
+            console.log("ERROR: " + image);
+            console.log("GPSAltitudeRef: " + tags.GPSAltitudeRef);
+            pass = false; 
+          } // Indicates the altitude used as the reference altitude. If the reference is sea level and the altitude is above sea level, 0 is given. If the altitude is below sea level, a value of 1 is given and the altitude is indicated as an absolute value in the GSPAltitude tag. The reference unit is meters. Note that this tag is BYTE type, unlike other reference tags.
+          if (isNaN(parseFloat(tags.GPSAltitude))) {
+            console.log("ERROR: " + image);
+            console.log("GPSAltitudeRef: " + tags.GPSAltitude);
+            pass = false;
+          } else {
+            // console.log("GPSAltitude: " + tags.GPSAltitude);
+            if (parseFloat(tags.GPSAltitude) < 10) { 
+              console.log("ERROR: " + image);
+              console.log("GPSAltitude: " + tags.GPSAltitude);
+              pass = false; 
+            } // Indicates the altitude based on the reference in GPSAltitudeRef. Altitude is expressed as one RATIONAL value. The reference unit is meters.
+          }
+          if (isNaN(parseFloat(tags.GPSLongitude)) === true || isNaN(parseFloat(tags.GPSLatitude)) === true ) { 
+            console.log("ERROR: " + image);
+            console.log("GPSLongitude: " + tags.GPSLongitude + " / " + "GPSLatitude: " + tags.GPSLatitude); 
+            pass = false; 
+          }
+          if (pass === true) {
+            var pt = turf.point([tags.GPSLongitude,tags.GPSLatitude]);
+            var count = 0;
+            for (var i = 0; i < aois.length; i++) {
+              var inside = turf.booleanPointInPolygon(pt, aois[i]);
+              if (inside) {
+                if (!aois[i].properties.images) {
+                  // need to create the array to hold the image names if it doesn't exist yet
+                  aois[i].properties.images = [];
+                }
+                // add the image to the array in the properties of the aoi polygon feature
+                aois[i].properties.images.push(image)
               }
-              // add the image to the array in the properties of the aoi polygon feature
-              aois[i].properties.images.push(image)
+              count++;
+              if (count == aois.length) {
+                cb(null);
+              }
             }
-            count++;
-            if (count == aois.length) {
-              cb(null);
-            }
+          } else {
+            cb(null)
           }
       });
 }
 
 function copyInImages(imgArray, destPath, cb) {
+  // TODO: feature might have an empty array
   var copyCount = 0;
   for (var i = 0; i < imgArray.length; i++) {
     var src = path.join(imgDir, imgArray[i]);
@@ -63,12 +106,12 @@ function copyInImages(imgArray, destPath, cb) {
 
 function createOutput() {
   var timestamp = moment().format("YYYY-MM-DD_HH-mm-ss");
-  fs.mkdirSync(path.join('output',timestamp));
-  // TODO: write full FeatureCollection
+  // fs.mkdirSync(path.join(outputPath,timestamp));
   var count = 0;
   for (var i = 0; i < aois.length; i++) {
     var indexValue = i;
-    var destPath = path.join('output',timestamp,indexValue.toString());
+    var destFolder = outputIdentifier + indexValue.toString();
+    var destPath = path.join(outputFolder, destFolder);
     fs.mkdirSync(destPath);
     // TODO: write FeatureCollection for just the one aoi
     copyInImages(aois[i].properties.images, destPath, function() {
